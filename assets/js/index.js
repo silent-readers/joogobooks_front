1950aa76cc719c7fc2c002c2ac76a6dd621242bd
simseulnyang <happysseul627@gmail.com>
2023-08-29 21:58:08 +0900

    ✨ Feat : index.js 코드 생성

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+const main_btn = document.querySelector('.enter-button');
+const active_ul = document.getElementsByClassName('menu');
+
+const payload = localStorage.getItem('payload');
+const parsed_payload = JSON.parse(payload);
+
+console.log(parsed_payload);
+
+if (!parsed_payload) {
+    main_btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        location.href="../assets/html/login.html";
+    })
+} else {
+    active_ul[0].classList.add('before')
+    active_ul[1].classList.remove('before')
+    active_ul[1].innerHTML = `
+        <li><img class="user" src="../assets/img/user.png" alt="user"></li>
+        <ul class="menu1">
+            <li>[별명]님, 환영합니다.</li>
+            <li class="menu1-1">
+                <a href="../assets/html/mypage.html">My Page</a>
+            </li>
+            <li><button type="button" id="logout-btn">Logout</button></li>
+        </ul>
+    `
+
+    const logout_btn = document.getElementById('logout-btn');
+    logout_btn.addEventListener('click', () => {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('payload');
+        alert("로그아웃 되었습니다.");
+        location.href="/index.html";
+    })
+
+    main_btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        location.href="../assets/html/shop.html"
+    })
+}
