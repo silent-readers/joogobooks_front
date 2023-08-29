d77bb08260cd0257648561f5b3a0b2230351d5bc
simseulnyang <happysseul627@gmail.com>
2023-08-29 14:08:56 +0900

    🐛 Fix : 파일명 수정

--- assets/js/Create Post.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const attachImageButton = document.getElementById("attachImageButton");
-  const imageInput = document.getElementById("imageInput");
-
-  attachImageButton.addEventListener("click", function () {
-    imageInput.click(); // 클릭 이벤트 발생
-  });
-});
