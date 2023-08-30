2fdfdb96666e8426e240bd773b1c1f1e099b3061
simseulnyang <happysseul627@gmail.com>
2023-08-30 15:21:57 +0900

    ✨ Feat : 작성한 유저일 경우 해당 정보 삭제 가능 버튼 추가

--- assets/js/bookdetail.js
+++ assets/js/bookdetail.js
@@ -3,6 +3,7 @@ window.onload = async function() {
     const urlParams = new URLSearchParams(window.location.search);
     const bookId = urlParams.get('id');
 
+    const access_token = localStorage.getItem('access_token')
     const payload = localStorage.getItem('payload');
     const parsed_payload = JSON.parse(payload)
     const username = parsed_payload.user_id
@@ -14,7 +15,8 @@ window.onload = async function() {
     const bookDetailDiv = document.getElementsByClassName('book-list')[0];
     const bookDetail = document.createElement('div');
     bookDetail.classList.add('book');
-    bookDetail.innerHTML = `
+
+    let innerHTMLContent = `
         <img src="${bookData.image}" alt="Book Cover"> 
         <div class="book-details">
             <h5>${bookData.sale_condition}</h5>
@@ -36,14 +38,42 @@ window.onload = async function() {
             </ul>
         </div>
     `;
+
     if (bookData.writer == username) {
-        bookDetail.innerHTML += `
-        <button class="chat"><a href="./updatePost.html?id=${bookData.id}">정보 수정하기</a></button>
+        innerHTMLContent += `
+        <a class="chat" href="./updatePost.html?id=${bookData.id}">정보 수정하기</a>
+        <button id="book_deletebtn" class="chat">정보 삭제하기</button>
         `
     } else {
-        bookDetail.innerHTML += `
+        innerHTMLContent += `
         <button class="chat"><a href="./Chat.html">채팅하기</a></button>
         `
     }
+    bookDetail.innerHTML = innerHTMLContent;
     bookDetailDiv.appendChild(bookDetail);
+
+    const del_btn = document.getElementById('book_deletebtn');
+    if (del_btn) {
+        del_btn.addEventListener('click', async function(e) {
+            e.preventDefault();
+            const response = await fetch(`http://127.0.0.1:8000/book/${bookData.id}/delete/`, {
+                headers: {
+                    'Authorization': `Bearer ${access_token}` 
+                },
+                method: 'DELETE',
+            })
+            .then((res) => {
+                console.log(res)
+                if (res.status === 204) {
+                    alert('해당 도서정보가 삭제되었습니다.')
+                    window.location.replace('http://127.0.0.1:5500/assets/html/shop.html')
+                } else {
+                    alert('해당 정보를 삭제할 수 없습니다!');
+                }
+            })
+            .catch((err) => {
+                alert(err);
+            })
+        });
+    }
 }
