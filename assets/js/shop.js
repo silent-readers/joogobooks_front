c21e16a4f3784897267818cb136a6438dbe838ae
simseulnyang <happysseul627@gmail.com>
2023-09-01 17:46:56 +0900

    🐛 Fix : server로 부터 media file 가져와 화면출력오류 수정

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,36 +1,39 @@
 window.onload = async function bookList() {
-    const response = await fetch('http://127.0.0.1:8000/book/list/', { method: 'GET' })
-    const response_json = await response.json()
+  const response = await fetch('http://127.0.0.1:8000/book/list/', { method: 'GET' })
+  const response_json = await response.json()
 
-    const book_list = document.getElementsByClassName('book-list')[0]; // book-list는 하나라고 가정하고 [0]으로 첫 번째 요소를 선택합니다.
+  const book_list = document.getElementsByClassName('book-list')[0]; // book-list는 하나라고 가정하고 [0]으로 첫 번째 요소를 선택합니다.
 
-    for (let bookData of response_json.results) {
-        let book = document.createElement('div');
-        book.className = 'book'; // 클래스 이름을 'book'으로 설정합니다.
+  for (let bookData of response_json.results) {
+      let book = document.createElement('div');
+      book.className = 'book'; // 클래스 이름을 'book'으로 설정합니다.
 
-        book.innerHTML = `
-        <input type="checkbox" class="checkbox">
-        <img src="${bookData.image}" alt="Book Cover" />
-        <div class="book-details">
-          <p class="book-name">${bookData.title}</p>
-          <ul class="book-info">
-            <li class="book2">${bookData.author} 저</li>
-            <li class="book2">${bookData.publisher}</li>
-            <li>EST Soft</li>
-          </ul>
-          <ul class="book-status">
-            <li class="book3">상태 : ${bookData.condition}</li>
-            <li class="book3">원가 : ${bookData.original_price}원</li>
-            <li>판매가 : ${bookData.selling_price}원</li>
-          </ul>
-        </div>
-        <ul class="info">
-          <li class="saler">판매자 : [별명] <!-- 판매자 별명 정보가 없어서 그대로 두었습니다. --></li>
+      const mediaUrl = 'http://127.0.0.1:8000';
+      const imageUrl = mediaUrl + bookData.image;
+
+      book.innerHTML = `
+      <input type="checkbox" class="checkbox">
+      <img src="${imageUrl}" alt="Book Cover" />
+      <div class="book-details">
+        <p class="book-name">${bookData.title}</p>
+        <ul class="book-info">
+          <li class="book2">${bookData.author} 저</li>
+          <li class="book2">${bookData.publisher}</li>
+          <li>EST Soft</li>
         </ul>
-        <button class="check-item"><a href="http://127.0.0.1:5500/assets/html/bookdetail.html?id=${bookData.id}/">상품 확인하기</a></button>
-        </div>
-        `;
+        <ul class="book-status">
+          <li class="book3">상태 : ${bookData.condition}</li>
+          <li class="book3">원가 : ${bookData.original_price}원</li>
+          <li>판매가 : ${bookData.selling_price}원</li>
+        </ul>
+      </div>
+      <ul class="info">
+        <li class="saler">판매자 : [별명] <!-- 판매자 별명 정보가 없어서 그대로 두었습니다. --></li>
+      </ul>
+      <a class="check-item" href="http://127.0.0.1:5500/assets/html/bookdetail.html?id=${bookData.id}/">상품 확인하기</a>
+      </div>
+      `;
 
-        book_list.appendChild(book);
-    }
+      book_list.appendChild(book);
+  }
 }
