9092ae8a4402c8fb186d647e522e38edd10d44e8
simseulnyang <happysseul627@gmail.com>
2023-09-14 21:15:06 +0900

    ♻️ Refactor : Pagination 적용을 위한 코드 리팩토링

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -2,16 +2,21 @@ import { backend } from "./url.js";
 import { frontend } from "./url.js";
 
 window.onload = async function bookList() {
+  let currentPage = 1;
   const payload = localStorage.getItem('payload')
   const parsed_payload = JSON.parse(payload)
 
 
-  const response = await fetch(backend + '/book/list/', { method: 'GET' })
-  const response_json = await response.json()
+  async function fetchBooks(page) {
+    const response = await fetch(backend + '/book/list/?page=' + page, { method: 'GET' })
+    const response_json = await response.json()
+    console.log(response_json);
 
-  const book_list = document.getElementsByClassName('book-list')[0]; // book-list는 하나라고 가정하고 [0]으로 첫 번째 요소를 선택합니다.
+    // book-list는 하나라고 가정하고 [0]으로 첫 번째 요소를 선택합니다.
+    const book_list = document.getElementsByClassName('book-list')[0];
+    book_list.innerHTML = "";
 
-  for (let bookData of response_json.results) {
+    for (let bookData of response_json.results) {
       let book = document.createElement('div');
       book.className = 'book'; // 클래스 이름을 'book'으로 설정합니다.
 
@@ -42,5 +47,50 @@ window.onload = async function bookList() {
       `;
 
       book_list.appendChild(book);
+    }
+
+    // Pagination btn 처리하기
+    const paginationDiv = document.getElementsByClassName('page_nation')[0];
+    paginationDiv.innerHTML = "";
+
+    // p-previous button
+    const pprevBtn = document.createElement('button');
+    pprevBtn.classList.add('pprev');
+    pprevBtn.onclick = () => { fetchBooks(1); };
+    paginationDiv.appendChild(pprevBtn);
+
+    // previous button
+    const prevBtn = document.createElement('button');
+    prevBtn.classList.add('prev');
+    prevBtn.onclick = () => { currentPage--; fetchBooks(currentPage); };
+    paginationDiv.appendChild(prevBtn);
+
+    // 페이지 번호 버튼
+    const totalPageCount = Math.ceil(response_json.count / 5);
+    console.log(totalPageCount);
+
+    for (let i = 1; i <= totalPageCount; i++) {
+      const pageBtn = document.createElement('button');
+      pageBtn.classList.add('page-btn');
+      pageBtn.innerText = i;
+      pageBtn.onclick = () => { currentPage = i; fetchBooks(currentPage); };
+      paginationDiv.appendChild(pageBtn);
+    }
+
+    // next button
+    const nextBtn = document.createElement('button');
+      nextBtn.classList.add('next');
+      nextBtn.onclick = () => { currentPage++; fetchBooks(currentPage); };
+      paginationDiv.appendChild(nextBtn);
+
+    // n-next button
+    const nnextBtn = document.createElement('button');
+    nnextBtn.classList.add('nnext');
+    nnextBtn.onclick = () => { fetchBooks(totalPageCount); };
+    paginationDiv.appendChild(nnextBtn);
+
   }
+
+  // 초기 로드에서 첫번째 페이지 불러오기
+  fetchBooks(currentPage);  
 }
