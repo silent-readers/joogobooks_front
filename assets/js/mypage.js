e94d30b1ec4e4f492c9dde6e3875553f8309b322
simseulnyang <happysseul627@gmail.com>
2023-09-20 15:56:13 +0900

    ♻️ Refactor : 프로필 수정 code 리팩토링

--- assets/js/mypage.js
+++ assets/js/mypage.js
@@ -1,96 +1,114 @@
+// JavaScript 파일
 import { backend } from "./url.js";
 import { frontend } from "./url.js";
 
-window.onload = async function() {
-  // URL에서 id 값을 추출합니다.
+// HTML 요소들을 가져옵니다.
+const imageInput = document.getElementById('imageInput');
+const profileImg = document.getElementById('my_img');
+const userImg = document.getElementById('my_profile_img');
+const nicknameInput = document.getElementById('nickname');
+const nickname = document.getElementById('profile-id');
+const aboutMeInput = document.getElementById('aboutme2');
+const aboutMe = document.getElementById('aboutme');
+const profileUpdateButton = document.getElementById('profile-putbtn');
+const attachImageButton = document.getElementById('attachImageButton'); // 이미지 변경 버튼 추가
 
-  const access_token = localStorage.getItem('access_token')
-  const payload = localStorage.getItem('payload');
-  const parsed_payload = JSON.parse(payload)
-  const userId = parsed_payload.user_id;
+// 이미지 변경 버튼 클릭 시 파일 업로드 창 열기
+attachImageButton.addEventListener('click', () => {
+  imageInput.click();
+});
 
-  document.getElementById("profile-id").innerText = parsed_payload.nickname;
+// 이미지 선택 시 미리보기 업데이트
+imageInput.addEventListener('change', () => {
+  if (imageInput.files.length > 0) {
+    const selectedFile = imageInput.files[0];
+    const reader = new FileReader();
 
-  // 프로필 이미지 불러오기
-  const profileImg = document.getElementById('my_img')
+    reader.onload = function(e) {
+      profileImg.src = e.target.result;
+    };
 
-  // 서버에서 프로필 이미지 URL 가져오기
-  fetch(`${backend}/api/user/profile/${userId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
+    reader.readAsDataURL(selectedFile);
+
+  } else {
+    console.log('파일이 선택되지 않았습니다.');
+  }
+});
+
+console.log(imageInput.files);
+
+// 사용자 정보를 불러오는 함수
+async function loadUserProfile() {
+  try {
+    const payload = localStorage.getItem('payload');
+    const parsed_payload = JSON.parse(payload);
+    const userId = parsed_payload.user_id;
+
+    // 사용자 정보를 서버에서 가져옵니다.
+    const response = await fetch(backend + `/api/user/profile/${userId}/`, { method: 'GET' });
+
+    if (!response.ok) {
+      const responseData = await response.json();
+      throw new Error(responseData.detail || '사용자 정보를 불러올 수 없습니다.');
     }
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.profile_img)
-    if (data.profile_img) {
-      // 이미지 URL이 있는 경우
-      const mediaUrl = backend;
-      const imageUrl = mediaUrl + data.profile_img;
-      console.log(imageUrl)
-      profileImg.src = imageUrl
-    } else {
-      // 이미지 URL이 없는 경우 기본 이미지 사용
-      profileImg.src = "../img/user.png";
-    }
-  })
-  .catch(error => {
-  });
 
+    const userData = await response.json();
 
-  // 이미지 첨부하기 버튼 이벤트
-  const attachImageBtn = document.getElementById('attachImageButton');
-  const imageInput = document.getElementById('imageInput');
-  const CovermyImg = document.getElementById('my_img');
+    // 가져온 사용자 정보를 화면에 표시합니다.
+    profileImg.src = backend + userData.profile_img;
+    userImg.src = backend + userData.profile_img;
 
-  attachImageBtn.addEventListener('click', () => {
-    imageInput.click();
-  })
+    nicknameInput.value = userData.nickname;
+    nickname.innerHTML = userData.nickname;
 
-  // 이미지 선택 시 미리보기 업데이트
-  imageInput.addEventListener('change', () => {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        CovermyImg.src =  e.target.result;
-      }
-      reader.readAsDataURL(imageInput.files[0]);
+    aboutMeInput.value = userData.about_me || '본인에 대한 한줄 소개를 작성해주세요.';
+    aboutMe.innerHTML = userData.about_me || '본인에 대한 한줄 소개를 작성해주세요.';
 
-    }
-  )
-
-  const profile_btn = document.getElementById('profile-putbtn');
- 
-  profile_btn.addEventListener('click', async function(e) {
-      e.preventDefault();
-
-      const formData = new FormData();
-
-      if (imageInput.files[0]) {
-          formData.append('profile_img', imageInput.files[0]);
-      }
-      formData.append('nickname', document.getElementById('nickname').value);
-      formData.append('about_me', document.getElementById('aboutme').value);
-
-      try {
-          const response = await fetch(`${backend}/api/user/profile/${userId}/create`, {
-              headers: {
-                  'Authorization': `Bearer ${access_token}`
-              },
-              method: 'POST',
-              body: formData
-          });
-
-          if (!response.ok) {
-            const responseData = await response.json(); // 서버에서 반환한 오류 메시지 파싱
-            throw new Error(responseData.detail || '프로필 생성이 실패했습니다.');
-          }
-
-          alert("프로필이 생성되었습니다.");
-          window.location.replace(frontend + '/assets/html/mypage_main.html');
-      } catch (error) {
-          alert(error.message || "오류가 발생했습니다.");
-          console.error(error);
-      }
-  });
+  } catch (error) {
+    alert(error.message || "오류가 발생했습니다.");
+    console.error(error);
+  }
 }
+
+// 페이지 로드 시 사용자 정보를 불러옵니다.
+window.onload = loadUserProfile;
+
+// 프로필 수정 버튼 클릭 시
+profileUpdateButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+
+  if (imageInput.files[0]) {
+    formData.append('profile_img', imageInput.files[0]);
+  }
+  formData.append('nickname', nicknameInput.value);
+  formData.append('about_me', aboutMeInput.value);
+
+  try {
+    const access_token = localStorage.getItem('access_token');
+    const payload = localStorage.getItem('payload');
+    const parsed_payload = JSON.parse(payload);
+    const userId = parsed_payload.user_id;
+
+    // 프로필 정보를 서버로 업데이트합니다.
+    const response = await fetch(`${backend}/api/user/profile/${userId}/update/`, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      },
+      method: 'PUT',
+      body: formData
+    });
+
+    if (!response.ok) {
+      const responseData = await response.json();
+      throw new Error(responseData.detail || '프로필 수정이 실패했습니다.');
+    }
+
+    alert("프로필이 수정되었습니다.");
+    window.location.replace(frontend + '/assets/html/mypage_main.html');
+  } catch (error) {
+    alert(error.message || "오류가 발생했습니다.");
+    console.error(error);
+  }
+});
